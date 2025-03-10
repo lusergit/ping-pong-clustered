f9c4365422cd8c12a830631d3a0c31a55bdad400
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-03-10 13:29:46 +0100

    App init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Multi cluster POC
+
+Two apps can communicate between them, using libcluster and horde
+
+# Preliminary steps
+
+``` sh
+mix deps.get
+mix deps.compile
+```
+
+# Usage
---
+++ apps/ping/lib/ping/application.ex
@@ -0,0 +1,20 @@
+defmodule Ping.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Ping.Worker.start_link(arg)
+      # {Ping.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Ping.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
